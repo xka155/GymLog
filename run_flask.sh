9f968665cd237ae74bb02e2cede656499924e78a
henry <henry.young155@gmail.com>
2017-08-20 22:58:57 +0100

    Server run script

--- run_flask.sh
+++ run_flask.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
+while getopts d option
+do
+ case "${option}"
+ in
+ d) DEBUG=${OPTARG};;
+ esac
+done
 
 export FLASK_APP=run.py
+
+if $DEBUG
+then
+    export FLASK_DEBUG=1
+fi
+
 flask run
