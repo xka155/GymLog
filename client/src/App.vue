3a9ce1ef635f78e6ab6c906ce7f351f7b2544426
xka155 <henry.young155@gmail.com>
2017-08-21 12:57:23 +0100

    Add vuetifyjs style.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <img src="./assets/logo.png">
+    <h1>FOFOF</h1>
     <router-view></router-view>
   </div>
 </template>
@@ -10,14 +11,3 @@ export default {
   name: 'app'
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
